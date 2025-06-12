30558ecdd57a435b30239c816cd6dfc142bf54f1
Ryan Neubauer <rjneub09@gmail.com>
2025-06-12 12:51:14 -0500

    Woooo exercise done (EZ)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 export default function App() {
+    const array1 = [1, 2, 3, 4, 5].map(x => x * x);
+    const names = ["alice", "bob", "charlie", "danielle"].map(name => {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    })
     return (
         <>
-            <h1>Hi! I am an app template!</h1>
+            <h1>{array1}</h1>
+            <h1>{names}</h1>
+            {["Ryan", "Carlo", "Brody"].map(name => <p>{name}</p>)}
         </>
     )
 }
