9732ed47acc40ba6bf38d0960522b18238ecd6e8
Ryan Neubauer <rjneub09@gmail.com>
2025-06-17 20:18:59 -0500

    Counter app that demonstrated parent child relationships

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import React from "react"
+import Count from "./Components/Count"
+
 export default function App() {
+    const [count, setCount] = React.useState(0)
+
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
+
     return (
-        <>
-            <h1>Hi! I am an app template!</h1>
-        </>
+        <main className="container">
+            <div className="counter">
+                <button
+                    className="minus"
+                    onClick={subtract}
+                    aria-label="Decrease count"
+                >-</button>
+
+                <Count number={count} />
+
+                <button
+                    className="plus"
+                    onClick={add}
+                    aria-label="Increase count"
+                >+</button>
+            </div>
+        </main>
     )
 }
